d2c5a47dfd3963948b29a0ee77281ab6b9abf8e0
Татьяна Алексеевна Мельникова <tanya@localhost.localdomain>
2020-04-16 20:38:25 +0300

    fix code-style

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import store from './store';
-import App from './app/App';
-import './styles/globalStyles.css';
-import * as serviceWorker from './utils/serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import store from './store'
+import App from './app/App'
+import './styles/globalStyles.css'
+import * as serviceWorker from './utils/serviceWorker'
 
-import { BrowserRouter } from 'react-router-dom';
-
-
-ReactDOM.render((
-	<BrowserRouter>
-		<Provider store={store}>
-			<App />
-		</Provider>
-	</BrowserRouter>),
-document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root'),
+)
 // If you want your app to work offline and load fat<>er, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
