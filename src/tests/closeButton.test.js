ee8770fce0f7533b5eef9ab4d6d771edaf226132
Татьяна Алексеевна Мельникова <tanya@localhost.localdomain>
2020-04-16 20:47:38 +0300

    fix

--- src/tests/closeButton.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import CloseButton from '../components/Buttons/CloseButton';
-
-it('renders correctly', () => {
-	const tree = renderer.create(<CloseButton handleModal={jest.fn()} />).toJSON();
-	expect(tree).toMatchSnapshot();
-});
