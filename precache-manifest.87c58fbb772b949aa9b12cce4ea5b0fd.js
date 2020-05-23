aacb407f60dff7d1911f2201f9fed5b2e3f559c1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-23 13:55:21 +0000

    Deploy tanya124/2019-2-Track-Frontend-T-Melnikova to github.com/tanya124/2019-2-Track-Frontend-T-Melnikova.git:gh-pages

---
+++ precache-manifest.87c58fbb772b949aa9b12cce4ea5b0fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecb8aee5fe06c71cd6dd111df6bc3971",
+    "url": "/2019-2-Track-Frontend-T-Melnikova/index.html"
+  },
+  {
+    "revision": "37e91921dcf06fb04c2f",
+    "url": "/2019-2-Track-Frontend-T-Melnikova/static/css/main.88544ca2.chunk.css"
+  },
+  {
+    "revision": "bded65af5d9c06beb086",
+    "url": "/2019-2-Track-Frontend-T-Melnikova/static/js/2.bca26b0d.chunk.js"
+  },
+  {
+    "revision": "ee7767de5c6be5c4c11c18999ea7353c",
+    "url": "/2019-2-Track-Frontend-T-Melnikova/static/js/2.bca26b0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37e91921dcf06fb04c2f",
+    "url": "/2019-2-Track-Frontend-T-Melnikova/static/js/main.f1a471f9.chunk.js"
+  },
+  {
+    "revision": "7bfa3c6e3048f86894ab",
+    "url": "/2019-2-Track-Frontend-T-Melnikova/static/js/runtime-main.1ed04e0e.js"
+  }
+]);
